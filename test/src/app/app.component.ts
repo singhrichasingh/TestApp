98a1d0a331a18be3fa8f92f8a17968ef31fc9a98
Richa Singh <richasingh@Divyanshus-MacBook-Air.local>
2024-02-20 23:17:08 +0530

    added init

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { Platform } from '@ionic/angular';
+declare var CameraCustom:any;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform) {
+    this.initializeApp();
+  }
+  initializeApp(){
+    this.platform.ready().then(() => {
+      alert("allllll")
+    CameraCustom.init(
+      (res:any) => {
+        alert("helll0  "+res)
+        //alert("1"+JSON.stringify(res))
+      }, (err:any) => {
+      alert("2,,,,"+JSON.stringify(err))
+      });
+  })
+  }
+ 
+
 }
